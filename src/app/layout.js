cc1fb3bb0635c7617ab2e91d63f955ea8055fd99
Melvin <melvinmps11301@gmail.com>
2025-06-05 10:49:48 +0300

    frontend updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
       <body>
         <Header />
         <SmoothScrollProvider>{children}</SmoothScrollProvider>
